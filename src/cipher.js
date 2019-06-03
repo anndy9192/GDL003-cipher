2135bda14d15f629264af30562f13238132ce199
andy9192 <andybmq@gmail.com>
2019-06-02 22:30:27 -0500

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,12 @@
 window.cipher = {
-  // ... 
+  function encode(offset, string) {
+    for(let i = 0 ; i < string.length ; i++){
+      console.log(codificado)
+      codificado=codificado + String.fromCharCode (( string.charCodeAt(i) - 65 + offset ) % 26 + 65)
+    }
+
+  }
+  // ...
 };
+
+module.exports = window.cipher;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+var x = require('./cipher.js')
+document.getElementById("codificar").addEventListener("click", () => {
+  alert("codificar")
+});
+
+document.getElementById("decodificar").addEventListener("click", () => {
+    alert("decodificar")
+});
